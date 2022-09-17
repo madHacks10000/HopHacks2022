d2869486e1a3127ba5f891971ca1dc511c3971df
shihd1 <46411183+shihd1@users.noreply.github.com>
2022-09-16 23:59:28 -0400

    Initial Firebase and Swift

---
+++ ReceiptsAndMore/ReceiptsAndMore/ReceiptsAndMoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptsAndMoreApp.swift
+//  ReceiptsAndMore
+//
+//  Created by MacBookPro on 2022/9/16.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptsAndMoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
